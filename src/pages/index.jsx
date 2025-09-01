6d5ccb545a05c26f28bf9a83d4067b408f0fa9de
DavidIsrael <davidisrael.rj@gmail.com>
2025-09-01 20:18:30 -0300

    Carrossel1

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+import Carrossel1 from "@/template/carrossel/carrossel1";
 import Carrosel3d from "@/template/carrossel/carrossel3d";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <div>Carrossel
 
       <Carrosel3d />
+      <Carrossel1/>
     </div>
   );
 }
---
+++ src/template/carrossel/carrossel1.jsx
@@ -0,0 +1,8 @@
+export default function Carrossel1(){
+    return(
+        <div>
+            Carrossel
+            
+        </div>
+    )
+}
