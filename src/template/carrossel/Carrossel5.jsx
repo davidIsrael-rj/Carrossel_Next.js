076fd7ab832a3c48afc07abe196d6404940b2d76
DavidIsrael <davidisrael.rj@gmail.com>
2025-09-16 20:08:37 -0300

    Verificando o tamanho da tela para exibir a quantidade de slider

--- src/template/carrossel/Carrossel5.jsx
+++ src/template/carrossel/Carrossel5.jsx
@@ -18,22 +18,47 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import { Swiper, SwiperSlide } from 'swiper/react'
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 
 export default function Carrossel5() {
+    const [slidePerView, setSlidePerView] = useState(3)
 
     const data = [
         { id: 1, image: mesa1 },
         { id: 2, image: mesa2 },
         { id: 3, image: mesa3 },
         { id: 4, image: predio },
+        { id: 5, image: predio2 },
+        { id: 6, image: predio3 },
+        { id: 7, image: reuniao1 },
+        { id: 8, image: computador1 },
     ]
+
+    useEffect(() => {
+
+        function handleResize() {
+            if (window.innerWidth < 720) {
+                setSlidePerView(1);
+            } else {
+                setSlidePerView(3);
+            }
+        }
+
+        handleResize();
+
+        window.addEventListener("resize", handleResize)
+
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, [])
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Carrossel5 com Swiper</h1>
             <Swiper
-                slidesPerView={1}
-                pagination={{clickable: true}}
+                slidesPerView={slidePerView}
+                pagination={{ clickable: true }}
                 navigation
             >
                 {data.map((item) => (
